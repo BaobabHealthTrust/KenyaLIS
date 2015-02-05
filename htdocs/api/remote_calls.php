c72e496e906a9f2304fbc3878d87ad172fd565b3
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2015-02-05 22:42:28 +0200

    Added api file to assist in making server queries remotely

---
+++ htdocs/api/remote_calls.php
@@ -0,0 +1,100 @@
+<?php
+
+  require_once "authenticate.php";
+
+  if(isset($_REQUEST['action'])) {
+  
+    // Expects calls made as: /api/remote_calls?action=get_test_type_measure&id=TEST_ID
+    if($_REQUEST['action'] == 'get_test_type_measure') {
+    
+      if(!isset($_REQUEST['id'])) {
+          echo -2;
+          return;
+      } else {
+      
+        getTestTypeMeasure($_REQUEST["id"]);
+        
+        return;
+      
+      }
+      
+    // Expects calls made as: /api/remote_calls?action=by_session_num&id=ACCESSION_NUMBER
+    } else if($_REQUEST['action'] == 'by_session_num') {  
+    
+      if(!isset($_REQUEST['id'])) {
+          echo -2;
+          return;
+      } else {
+      
+        getBySessionNum($_REQUEST["id"]);
+        
+        return;
+      
+      }
+    
+    } else if($_REQUEST['action'] == 'get_patient_by_sp_id') {  
+    
+      if(!isset($_REQUEST['id'])) {
+          echo -2;
+          return;
+      } else {
+      
+        getPatientBySpId($_REQUEST["id"]);
+        
+        return;
+      
+      }
+    
+    }
+    
+  }
+
+  function getTestTypeMeasure($id){
+  
+    $response = get_test_type_measure($id);
+  
+    echo json_encode($response);
+    
+  }
+  
+  function getBySessionNum($q){
+  
+    $specimens = search_specimens_by_session_exact($q);
+    
+    $result = array();
+    
+    for($i = 0; $i < count($specimens); $i++) {
+    
+      $id = $specimens[$i]->specimenId;
+      
+      $tests = get_tests_by_specimen_id($id);
+      
+      $type = get_specimen_type_by_id($specimens[$i]->specimenTypeId);
+    
+      for($t = 0; $t < count($tests); $t++) {
+      
+        $test = get_test_type_measure($tests[$t]->testTypeId);
+        
+        $name = get_test_name_by_id($tests[$t]->testTypeId);
+        
+        $result["$name|".$tests[$t]->testTypeId."|".$type->name] = $test;
+      
+      }
+      
+    }
+    
+    echo json_encode($result);
+    
+  }
+
+  function getPatientBySpId($id){
+  
+    $spec = get_specimens_by_session($id);
+  
+    $response = get_patient_by_sp_id($spec[0]->specimenId);
+    
+    echo json_encode($response);
+  
+  }
+
+?>
