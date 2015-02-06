9be4e99ea160c45118e6119e88d357940bb59c1f
Kenneth <kennethkapundi1@gmail.com>
2015-02-06 15:08:14 +0200

    API patient lookup nw using National ID

--- htdocs/api/get_patient_specimen_details.php
+++ htdocs/api/get_patient_specimen_details.php
@@ -5,9 +5,13 @@ require_once "authenticate.php";
 $debug = false;
 
 
-$patient_id =  $_REQUEST['patient_id'];
+$patient_id =  $_REQUEST["npid"];
+$patient = get_patient_by_npid($patient_id);
+$result = false;
 
-$result = API::get_patient_specimen_details(1);
+if ($patient && $patient != NULL && $patient->patientId != ""){
+    $result = API::get_patient_specimen_details($patient->patientId);
+}
 
 echo json_encode($result);
 ?>
