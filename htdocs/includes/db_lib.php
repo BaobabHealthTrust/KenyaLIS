817ce63cb378af916c197f2d105781964f4fb434
Kenneth <kennethkapundi1@gmail.com>
2015-02-27 09:30:14 +0200

    removed received specimens in lab depart dashboard

--- htdocs/includes/db_lib.php
+++ htdocs/includes/db_lib.php
@@ -16687,7 +16687,7 @@ class API
     	}else if ($dashboard_type == 'ward'){
     		$status = "'Ordered', 'Drawn', 'Rejected', 'Tested', 'Verified'";
 		}else if ($dashboard_type == 'labdepartment'){
-			$status = "'Received At Reception', 'Testing', 'Received In Department'";   		   		
+			$status = "'Received At Reception', 'Received In Department'";   		   		
     	}else{
     		$status = $params['status'];
     	}
