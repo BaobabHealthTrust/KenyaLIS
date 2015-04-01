8ead46e739d9c29ab9bee15ea965ada961b9ca3c
Kenneth <kennethkapundi1@gmail.com>
2015-04-01 14:20:12 +0200

    added metadata file

---
+++ metadata_table_list.sql
@@ -0,0 +1,1 @@
+mysqldump -u root -ppassword blis_revamp --tables test_type measure test_type_measure test_panel test_type_container_type specimen_type specimen_test measure_mapping specimen_custom_data test_category unit > metadata.sql
